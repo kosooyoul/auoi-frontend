23120a8a8aca5fabb94364d8bc7bc482a56fe2e1
kosooyoul <kosooyoul@gmail.com>
2022-06-03 23:54:43 +0900

    🔨 Upt. 맵 편집 툴 작업

--- src/cell.js
+++ src/cell.js
@@ -15,8 +15,49 @@ class Cell {
 	};
 	rotation = 0;
 
-	constructor() {
+	constructor(options) {
+		if (options == null) {
+			return;
+		}
 
+		if (options.position && options.size) {
+			this._position.x = options.position.x;
+			this._position.y = options.position.y;
+			this._position.z = options.position.z;
+			
+			this._offset.x = ((options.size.w >> 1) * this._position.x - (options.size.w >> 1) * this._position.y)
+			this._offset.y = ((options.size.h >> 1) * this._position.y + (options.size.h >> 1) * this._position.x - this._position.z * options.size.d);
+		}
+
+		if (options.texture) {
+			this._texture.top = options.texture.top;
+			this._texture.left = options.texture.left;
+			this._texture.right = options.texture.right;
+		}
+
+		this._prop = options.prop;
+		this._effect = options.effect;
+		this._label = options.label;
+
+		if (options.action) {
+			if (options.action.all) {
+				this._action.all = options.action.all;
+			} else {
+				this._action.top = options.action.top;
+				this._action.left = options.action.left;
+				this._action.right = options.action.right;
+			}
+		}
+
+		if (options.alpha) {
+			this._alpha = options.alpha;
+		}
+
+		if (options.timeOffset) {
+			this._timeOffset = options.timeOffset;
+		} else {
+			this._timeOffset = Math.random() * 60000;
+		}
 	}
 
 	// TODO: Refactoring
@@ -72,35 +113,30 @@ class Cell {
 		this.focused = onOff;
 	}
 
-	setProp(prop) {
-		this.prop = prop;
-		return this;
-	}
-
-	setLabel(label) {
-		this.label = label;
-		return this;
-	}
-
-	isFrontOf(targetCell) {
-		return (this.rotatedPosition.z - targetCell.rotatedPosition.z) || (this.rotatedPosition.y - targetCell.rotatedPosition.y) || (this.rotatedPosition.x - targetCell.rotatedPosition.x);
-	}
-
 	pick(x, y) {
-		var rx = x - this.offset.x; // (40 * this.rotatedPosition.x - 40 * this.rotatedPosition.y);
-		var ry = y - this.offset.y; // (20 * this.rotatedPosition.y + 20 * this.rotatedPosition.x - this.rotatedPosition.z * 35);
+		var rx = x - this._offset.x;
+		var ry = y - this._offset.y;
 
-		// var p1 = {x: 0, y: -20};
-		// var p2 = {x: -40, y: 0};
-		// var p3 = {x: 0, y: 20};
-		// var p4 = {x: 40, y: 0};
+		if (this._texture.top) {
+			if (this._pickTop(rx, ry)) {
+				return "top";
+			}
+		}
+		if (this._texture.left) {
+			if (this._pickLeft(rx, ry)) {
+				return "left";
+			}
+		}
+		if (this._texture.right) {
+			if (this._pickRight(rx, ry)) {
+				return "right";
+			}
+		}
 
-		// var r1 = -0.5; // (p2.y - p1.y) / (p2.x - p1.x);
-		// var r3 = -0.5; // (p4.y - p3.y) / (p4.x - p3.x);
+		return null;
+	}
 
-		// var r2 = 0.5; // (p3.y - p2.y) / (p3.x - p2.x);
-		// var r4 = 0.5; // (p1.y - p4.y) / (p1.x - p4.x);
-
+	_pickTop(rx, ry) {
 		if (-0.5 * rx + -20 <= ry && -0.5 * rx + 20 >= ry
 		 && 0.5 * rx - 20 <= ry && 0.5 * rx - -20 >= ry) {
 			return true;
@@ -109,9 +145,10 @@ class Cell {
 		return false;
 	}
 
-	pickLeft(x, y) {
-		var rx = x - this.offset.x; // (40 * this.rotatedPosition.x - 40 * this.rotatedPosition.y);
-		var ry = y - this.offset.y; // (20 * this.rotatedPosition.y + 20 * this.rotatedPosition.x - this.rotatedPosition.z * 35);
+	_pickLeft(rx, ry) {
+		if (!this._texture.left) {
+			return false;
+		}
 
 		var p1 = {x: 0, y: 20};
 		var p2 = {x: -40, y: 0};
@@ -130,9 +167,10 @@ class Cell {
 		return false;
 	}
 
-	pickRight(x, y) {
-		var rx = x - this.offset.x; // (40 * this.rotatedPosition.x - 40 * this.rotatedPosition.y);
-		var ry = y - this.offset.y; // (20 * this.rotatedPosition.y + 20 * this.rotatedPosition.x - this.rotatedPosition.z * 35);
+	_pickRight(rx, ry) {
+		if (!this._texture.right) {
+			return false;
+		}
 
 		var p1 = {x: 40, y: 0};
 		var p2 = {x: 0, y: 20};
@@ -149,6 +187,123 @@ class Cell {
 		}
 
 		return false;
+	}
+
+	getOffset() {
+		return this._offset;
+	}
+
+	getOffset() {
+		return this._offset;
+	}
+
+	getTexture() {
+		return this._texture;
+	}
+
+	getStatus() {
+		return this._status;
+	}
+
+	getProp() {
+		return this._prop;
+	}
+
+	getLabel() {
+		return this._label;
+	}
+
+	getEffect() {
+		return this._effect;
+	}
+
+	getAlpha() {
+		return this._alpha;
+	}
+
+	getTimeOffset() {
+		return this._timeOffset;
+	}
+
+	setProp(prop) {
+		this._prop = prop;
+	}
+
+	setLabel(label) {
+		this._label = label;
+	}
+
+	setEffect(effect) {
+		this._effect = effect;
+	}
+
+	setStatus(status, side) {
+		if (side == "top") {
+			this._status.top = status;
+		} else if (side == "left") {
+			this._status.left = status;
+		} else if (side == "right") {
+			this._status.right = status;
+		} else {
+			this._status.top = status;
+			this._status.left = status;
+			this._status.right = status;
+		}
+	}
+
+	resetStatus(side) {
+		if (side == "top") {
+			this._status.top = null;
+		} else if (side == "left") {
+			this._status.left = null;
+		} else if (side == "right") {
+			this._status.right = null;
+		} else {
+			this._status.top = null;
+			this._status.left = null;
+			this._status.right = null;
+		}
+	}
+
+	getAction(side) {
+		if (side == "top") {
+			return this._action.top || this._action.all;
+		} else if (side == "left") {
+			return this._action.left || this._action.all;
+		} else if (side == "right") {
+			return this._action.right || this._action.all;
+		} else if (side == "all") {
+			return this._action.all;
+		}
+		return null;
+	}
+
+	isFrontOf(targetCell) {
+		return (this.rotatedPosition.z - targetCell.rotatedPosition.z) || (this.rotatedPosition.y - targetCell.rotatedPosition.y) || (this.rotatedPosition.x - targetCell.rotatedPosition.x);
+	}
+
+	static getBlocksBoundary(blocks) {
+		var boundary = {
+			left: 0,
+			right: 0,
+			top: 0,
+			bottom: 0
+		};
+
+		blocks.forEach(block => {
+			boundary.left = Math.min(boundary.left, block._offset.x);
+			boundary.right = Math.max(boundary.right, block._offset.x);
+			boundary.top = Math.min(boundary.top, block._offset.y);
+			boundary.bottom = Math.max(boundary.bottom, block._offset.y);
+		});
+
+		return boundary;
+	}
+
+	static sortBlocks(blocks) {
+		return blocks.sort((a, b) => {
+			return (a._position.x - b._position.x) || (a._position.y - b._position.y);
+		});
 	}
 
 	draw(context) {
