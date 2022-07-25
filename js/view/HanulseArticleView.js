808c7e9dbb742ea3ffce58283da77adf634854a0
kosooyoul <kosooyoul@gmail.com>
2022-07-26 00:22:33 +0900

    🐛 Fix. display article detail time

--- js/view/HanulseArticleView.js
+++ js/view/HanulseArticleView.js
@@ -201,7 +201,7 @@ class HanulseArticleView extends HanulseOverlayView {
 		const year = ("000" + date.getFullYear()).slice(-4);
 		const month = ("0" + (date.getMonth() + 1)).slice(-2);
 		const dom = ("0" + date.getDate()).slice(-2);
-		const hours = ("0" + (date.getHours() + 1)).slice(-2);
+		const hours = ("0" + date.getHours()).slice(-2);
 		const minutes = ("0" + date.getMinutes()).slice(-2);
 		const seconds = ("0" + date.getSeconds()).slice(-2);
 		return year + "-" + month + "-" + dom + " " + hours + ":" + minutes + ":" + seconds;
