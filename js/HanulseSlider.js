6f878fc53b9c9ce143a1f3b0aa691047451e292d
kosooyoul <kosooyoul@gmail.com>
2023-06-27 13:08:39 +0900

    Upt. slider recording and moving fps

--- js/HanulseSlider.js
+++ js/HanulseSlider.js
@@ -204,7 +204,7 @@ class HanulseSlider {
 			}
 
 			self.records.push({x: self.offsetX, y: self.offsetY});
-		}, 1000 / 50)
+		}, 1000 / 30)
 	};
 
 	stopRecording() {
@@ -275,7 +275,7 @@ class HanulseSlider {
 			if (Math.abs(dx) < 0.1 && Math.abs(dy) < 0.1) {
 				self.stopSliding();
 			}
-		}, 1000 / 50);
+		}, 1000 / 60);
 	};
 
 	stopSliding() {
