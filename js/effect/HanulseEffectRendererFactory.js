0845e211ed1ff46690cd12a73294749ef6f1acc5
kosooyoul <kosooyoul@gmail.com>
2023-07-01 10:03:55 +0900

    Add. energy effect and fix bug

--- js/effect/HanulseEffectRendererFactory.js
+++ js/effect/HanulseEffectRendererFactory.js
@@ -3,6 +3,7 @@ class HanulseEffectRendererFactory {
 	linkEffectRenderer = null;
 	prismEffectRenderer = null;
 	warpEffectRenderer = null;
+	energyEffectRenderer = null;
 
 	constructor() {
 
@@ -11,10 +12,11 @@ class HanulseEffectRendererFactory {
 	get(effectName) {
 		switch (effectName) {
 			case "flash": return this.flashEffectRenderer || (this.flashEffectRenderer = new HanulseFlashEffectRenderer());
-			case "link": return this.linkEffectRenderer || (this.LinkEffectRenderer = new HanulseLinkEffectRenderer());
+			case "link": return this.linkEffectRenderer || (this.linkEffectRenderer = new HanulseLinkEffectRenderer());
 			case "prism": return this.prismEffectRenderer || (this.prismEffectRenderer = new HanulsePrismEffectRenderer());
-			case "warp": return this.warpEffectRenderer || (this.WarpEffectRenderer = new HanulseWarpEffectRenderer());
-			case "bigwarp": return this.warpEffectRenderer || (this.WarpEffectRenderer = new HanulseWarpEffectRenderer());
+			case "warp": return this.warpEffectRenderer || (this.warpEffectRenderer = new HanulseWarpEffectRenderer());
+			case "energy": return this.energyEffectRenderer || (this.energyEffectRenderer = new HanulseEnergyEffectRenderer());
+			case "bigwarp": return this.warpEffectRenderer || (this.warpEffectRenderer = new HanulseWarpEffectRenderer());
 		}
 		return null;
 	}
---
+++ js/effect/HanulseEnergyEffectRenderer.js
@@ -0,0 +1,87 @@
+class HanulseEnergyEffectRenderer {
+	render(context, _timeOffset) {
+		context.save();
+
+		context.translate(0, -20);
+
+		this.drawMainBall(context, 0, 1.5, 2.0);
+		this.drawMainBall(context, 0, 1.2, 1.0);
+		this.drawMainBall(context, 0, 0.8, 2.4);
+		this.drawEnergyBall(context, 0, 1.0, 1.0, 0);
+		this.drawEnergyBall(context, 100, 1.2, 2, 0.2);
+		this.drawEnergyBall(context, 500, 1.5, 1.2, 0.4);
+		this.drawEnergyBall(context, 600, 0.7, 1.6, 0.5);
+		this.drawEnergyBall(context, 700, 0.5, 1.9, 0.3);
+		this.drawEnergyBall(context, 300, 1.3, 1.2, 0.1);
+		this.drawEnergyBall(context, 400, 1.4, 2.0, 1.7);
+		this.drawEnergyBall(context, 450, 0.9, 1.8, 1.8);
+		this.drawEnergyBall(context, 650, 1.0, 1.2, 2.6);
+		this.drawEnergyBall(context, 800, 0.2, 0.2, 2.4);
+		this.drawEnergyBall(context, 900, 3.2, 0.8, 1.3);
+		this.drawEnergyBall(context, 450, 0.4, 1.8, 1.2);
+		this.drawEnergyBall(context, 650, 0.7, 1.2, 2.9);
+		this.drawEnergyBall(context, 800, 2.2, 0.2, 2.8);
+		this.drawEnergyBall(context, 600, 0.9, 0.6, 1.5);
+		this.drawEnergyBall(context, 700, 0.7, 0.9, 1.3);
+		this.drawEnergyBall(context, 300, 1.4, 1.7, 2.1);
+		this.drawEnergyBall(context, 400, 1.6, 2.2, 2.7);
+		this.drawEnergyBall(context, 450, 0.9, 0.8, 0.8);
+		this.drawEnergyBall(context, 650, 1.2, 0.6, 1.6);
+		this.drawEnergyBall(context, 800, 0.4, 1.2, 3.0);
+		this.drawEnergyBall(context, 900, 2.2, 1.8, 3.1);
+
+		context.restore();
+	}
+
+	drawMainBall(context, timeOffset, timeScale, scale) {
+		var tick = Date.now() + timeOffset;
+		var ratio = ((tick * timeScale) % 2000) / 2000;
+
+		var curvedRatio = Math.sin(ratio * Math.PI);
+
+		var alpha = (1 - (curvedRatio * 2 - 1) * (curvedRatio * 2 - 1)) * 0.4 + 0.2;
+
+		context.globalAlpha = alpha;
+
+		context.save();
+
+		context.scale(scale + curvedRatio, scale + curvedRatio);
+
+		context.save();
+		context.beginPath();
+		context.arc(0, 0, 2, 0, Math.PI * 2);
+		context.fillStyle = "rgba(255, 255, 255, 1)";
+		context.fill();
+		context.closePath();
+		context.restore();
+
+		context.restore();
+	}
+
+	drawEnergyBall(context, timeOffset, timeScale, scale, rotate) {
+		var tick = Date.now() + timeOffset;
+		var ratio = ((tick * timeScale) % 2000) / 2000;
+
+		var curvedRatio = Math.sin(ratio * Math.PI);
+
+		var alpha = (1 - (curvedRatio * 2 - 1) * (curvedRatio * 2 - 1)) * 1;
+
+		context.globalAlpha = alpha;
+
+		context.save();
+
+		context.scale(curvedRatio, curvedRatio);
+		context.scale(scale, scale);
+
+		context.save();
+		context.beginPath();
+		context.rotate(ratio * Math.PI * 2 + rotate);
+		context.arc(10, 0, 2, 0, Math.PI * 2);
+		context.fillStyle = "rgba(255, 255, 255, 1)";
+		context.fill();
+		context.closePath();
+		context.restore();
+
+		context.restore();
+	}
+}
