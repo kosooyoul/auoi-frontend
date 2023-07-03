6905dc6786f88709be1599b23153909355b51483
kosooyoul <kosooyoul@gmail.com>
2023-07-03 23:27:08 +0900

    Upt. html template fetcher

--- js/html/HtmlTemplate.js
+++ js/html/HtmlTemplate.js
@@ -1,20 +1,22 @@
 class HtmlTemplate {
 	static _templates = {};
-
-	static _loadTemplate(url, async, callback) {
-		const result = $.ajax({"url": url, "async": async, "success": callback});
-		this._templates[url] = result && result.responseText;
-		return this._templates[url];
-	}
 	
 	static get(url) {
-		return this._templates[url] || this._loadTemplate(url, false);
+		if (this._templates[url]) {
+			return this._templates[url];
+		}
+		const result = $.ajax({"url": url, "async": false});
+		this._templates[url] = result && result.responseText;
+		return this._templates[url];
 	}
 
 	static fetch(url, callback) {
 		if (this._templates[url]) {
 			return callback(this._templates[url]);
 		}
-		return this._loadTemplate(url, true, (data) => callback(data));
+		$.ajax({"url": url, "async": true, "success": (data) => {
+			this._templates[url] = data;
+			callback(data);
+		}});
 	}
 }
