d7fe632a9f3dbf6341a95bac2f081050a7182e9f
kosooyoul <kosooyoul@gmail.com>
2023-07-22 22:39:18 +0900

    Test. board view, finish drawing

--- js/view/board/HanulseBoardView.js
+++ js/view/board/HanulseBoardView.js
@@ -726,15 +726,15 @@ class HanulseBoardView {
 			return;
 		}
 		
-		alert(0);
-		var pointer = event.targetTouches? event.targetTouches[0] : event;
-		var offset = this.$parent.offset();
-		var x = pointer.pageX - offset.left;
-		var y = pointer.pageY - offset.top;
+		// alert(0);
+		// var pointer = event.targetTouches? event.targetTouches[0] : event;
+		// var offset = this.$parent.offset();
+		// var x = pointer.pageX - offset.left;
+		// var y = pointer.pageY - offset.top;
 
-		// this.drawingPath.push({ x: x, y: y });
+		// // this.drawingPath.push({ x: x, y: y });
 
-		this.drawingCanvas.remove();
+		// this.drawingCanvas.remove();
 
 		/*
 		if (this.drawingPath.length > 2) {
