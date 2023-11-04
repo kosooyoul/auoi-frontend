2c820be71225c5d32430b7e5dfc5aa6936890d67
kosooyoul <kosooyoul@gmail.com>
2023-11-04 21:08:27 +0900

    Upt. big warp effect class syntax

--- js/effect/HanulseBigWarpEffectRenderer.js
+++ js/effect/HanulseBigWarpEffectRenderer.js
@@ -1,114 +1,127 @@
-class HanulseBigWarpEffectRenderer {
-	curvedRatio = new Array(100).fill(0).reduce((result, _v, i) => (result[i] = Math.sin(i / 100 * Math.PI), result), {});
+class HanulseEffectRenderer {
+	init(context) {}
+	render(context, timeOffset) {}
+}
 
-	initialized = false;
-	centerGradient = null;
-	ringGradient = null;
+class HanulseBigWarpEffectRenderer extends HanulseEffectRenderer {
+	static #curvedRatio;
 
-	init(context) {
-		this.centerGradient = context.createLinearGradient(0, 0, 0, -120);
-		this.centerGradient.addColorStop(0, "rgba(255, 255, 255, 1)");
-		this.centerGradient.addColorStop(1, "rgba(255, 255, 255, 0)");
-
-		this.ringGradient = context.createLinearGradient(0, -20, 0, 40);
-		this.ringGradient.addColorStop(0, "rgba(255, 255, 255, 1)");
-		this.ringGradient.addColorStop(1, "rgba(255, 255, 255, 0)");
-
-		this.initialized = true;
+	static {
+		this.#curvedRatio = new Array(100).fill(0).reduce((result, _v, i) => {
+			result[i] = Math.sin(i / 100 * Math.PI);
+			return result;
+		}, []);
 	}
 
+	#initialized = false;
+	#centerGradient = null;
+	#ringGradient = null;
+
+	/** @override */
+	init(context) {
+		this.#centerGradient = context.createLinearGradient(0, 0, 0, -120);
+		this.#centerGradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+		this.#centerGradient.addColorStop(1, "rgba(255, 255, 255, 0)");
+
+		this.#ringGradient = context.createLinearGradient(0, -20, 0, 40);
+		this.#ringGradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+		this.#ringGradient.addColorStop(1, "rgba(255, 255, 255, 0)");
+
+		this.#initialized = true;
+	}
+
+	/** @override */
 	render(context, timeOffset) {
-		if (this.initialized == false) {
+		if (this.#initialized == false) {
 			this.init(context);
 		}
 
-		var tick = Date.now() + timeOffset;
-		var ratio = (tick % 1000) / 1000;
-		var curvedRatio = this.curvedRatio[Math.floor(ratio * 100)];
-		var sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
+		const tick = Date.now() + timeOffset;
+		const ratio = (tick % 1000) / 1000;
+		const curvedRatio = HanulseBigWarpEffectRenderer.#curvedRatio[Math.floor(ratio * 100)];
+		const sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
 
-		var alpha = sqrtCurvedRatio * 0.8;
-		var colorDegree = sqrtCurvedRatio * 255;
-		var subColorDegree = (curvedRatio * curvedRatio) * 255;
-		var colorIndex = Math.floor(tick / 2000) % 6;
-		var subColorIndex = Math.floor(tick / 3000) % 6;
-
+		const alpha = sqrtCurvedRatio * 0.8;
+		const colorDegree = sqrtCurvedRatio * 255;
+		const subColorDegree = (curvedRatio * curvedRatio) * 255;
+		const colorIndex = Math.floor(tick / 2000) % 6;
+		const subColorIndex = Math.floor(tick / 3000) % 6;
 		
-		var color = this.getColor(colorIndex, colorDegree, subColorDegree);
-		var subColor = this.getColor(subColorIndex, colorDegree, subColorDegree);
+		const color = this.#getColor(colorIndex, colorDegree, subColorDegree);
+		const subColor = this.#getColor(subColorIndex, colorDegree, subColorDegree);
 
 		context.save();
 
 		context.globalAlpha = alpha;
 
 		context.save();
-		this.renderCenter(context, color, 740, 0);
+		this.#renderCenter(context, color, 740, 0);
 		context.restore();
 
 		context.save();
-		this.renderCenter(context, subColor, 1000, 400);
+		this.#renderCenter(context, subColor, 1000, 400);
 		context.restore();
 
 		context.save();
-		this.renderRing(context, 800, 0, 1.6, 24);
+		this.#renderRing(context, 800, 0, 1.6, 24);
 		context.restore();
 
 		context.save();
-		this.renderRing(context, 800, 400, 1.2, 10);
+		this.#renderRing(context, 800, 400, 1.2, 10);
 		context.restore();
 
 		context.save();
-		this.renderRing(context, 1100, 600, 1.2, 10);
+		this.#renderRing(context, 1100, 600, 1.2, 10);
 		context.restore();
 
 		context.save();
-		this.renderRing(context, 980, 700, 1.4, 40);
+		this.#renderRing(context, 980, 700, 1.4, 40);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 650, 400, -12, 10, 5);
+		this.#renderBar(context, 650, 400, -12, 10, 5);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 1200, 1400, -24, 3, 20);
+		this.#renderBar(context, 1200, 1400, -24, 3, 20);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 800, 500, -44, 5, 30);
+		this.#renderBar(context, 800, 500, -44, 5, 30);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 700, 1200, -56, 10, 15);
+		this.#renderBar(context, 700, 1200, -56, 10, 15);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 900, 5400, 14, 0, 10);
+		this.#renderBar(context, 900, 5400, 14, 0, 10);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 1000, 800, 34, 5, 20);
+		this.#renderBar(context, 1000, 800, 34, 5, 20);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 550, 300, 48, 10, 5);
+		this.#renderBar(context, 550, 300, 48, 10, 5);
 		context.restore();
 
 		context.save();
-		this.renderBar(context, 750, 500, 52, 10, 10);
+		this.#renderBar(context, 750, 500, 52, 10, 10);
 		context.restore();
 
 		context.restore();
 	}
 
-	renderCenter(context, color, duration, timeOffset) {
-		var ratio = ((Date.now() - timeOffset) % duration) / duration;
-		var curvedRatio = this.curvedRatio[Math.floor(ratio * 100)];
-		var sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
+	#renderCenter(context, color, duration, timeOffset) {
+		const ratio = ((Date.now() - timeOffset) % duration) / duration;
+		const curvedRatio = HanulseBigWarpEffectRenderer.#curvedRatio[Math.floor(ratio * 100)];
+		const sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
 
-		var offsetY = 120;
-		var width = 40;
-		var height = 20;
-		var alpha = sqrtCurvedRatio * 0.4 + 0.1;
+		const offsetY = 120;
+		const width = 40;
+		const height = 20;
+		const alpha = sqrtCurvedRatio * 0.4 + 0.1;
 
 		context.globalAlpha = alpha;
 
@@ -125,26 +138,26 @@ class HanulseBigWarpEffectRenderer {
 		context.closePath();
 	}
 
-	renderRing(context, duration, timeOffset, widthScale, offsetY) {
-		var tick = Date.now() - timeOffset;
-		var ratio = (tick % duration) / duration;
-		var halfCurvedRatio = this.curvedRatio[Math.floor(ratio * 50)];
-		var curvedRatio = this.curvedRatio[Math.floor(ratio * 100)];
-		var sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
+	#renderRing(context, duration, timeOffset, widthScale, offsetY) {
+		const tick = Date.now() - timeOffset;
+		const ratio = (tick % duration) / duration;
+		const halfCurvedRatio = HanulseBigWarpEffectRenderer.#curvedRatio[Math.floor(ratio * 50)];
+		const curvedRatio = HanulseBigWarpEffectRenderer.#curvedRatio[Math.floor(ratio * 100)];
+		const sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
 
-		var colorDegree = halfCurvedRatio * 255;
-		var subColorDegree = (curvedRatio * curvedRatio) * 255;
-		var colorIndex = Math.floor(tick / 2000) % 6;
-		var color = this.getColor(colorIndex, colorDegree, subColorDegree);
+		const colorDegree = halfCurvedRatio * 255;
+		const subColorDegree = (curvedRatio * curvedRatio) * 255;
+		const colorIndex = Math.floor(tick / 2000) % 6;
+		const color = this.#getColor(colorIndex, colorDegree, subColorDegree);
 
-		var target = -140;
-		var translateY = target * halfCurvedRatio;
+		const target = -140;
+		const translateY = target * halfCurvedRatio;
 
 		context.translate(0, translateY + 20);
 
-		var scaledWidth = 40 * widthScale;
-		var scaledHeight = 20 * widthScale;
-		var alpha = sqrtCurvedRatio * 0.4;
+		const scaledWidth = 40 * widthScale;
+		const scaledHeight = 20 * widthScale;
+		const alpha = sqrtCurvedRatio * 0.4;
 
 		context.globalAlpha = alpha;
 
@@ -173,24 +186,19 @@ class HanulseBigWarpEffectRenderer {
 		context.closePath();
 	}
 
-	renderBar(context, duration, timeOffset, offsetX, offsetY, height) {
-		var tick = Date.now() - timeOffset;
-		var ratio = (tick % duration) / duration;
-		var halfCurvedRatio = this.curvedRatio[Math.floor(ratio * 50)];
-		var curvedRatio = this.curvedRatio[Math.floor(ratio * 100)];
-		var sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
+	#renderBar(context, duration, timeOffset, offsetX, offsetY, height) {
+		const tick = Date.now() - timeOffset;
+		const ratio = (tick % duration) / duration;
+		const halfCurvedRatio = HanulseBigWarpEffectRenderer.#curvedRatio[Math.floor(ratio * 50)];
+		const curvedRatio = HanulseBigWarpEffectRenderer.#curvedRatio[Math.floor(ratio * 100)];
+		const sqrtCurvedRatio = (1 - (curvedRatio - 1) * (curvedRatio - 1));
 
-		var colorDegree = halfCurvedRatio * 255;
-		var subColorDegree = (curvedRatio * curvedRatio) * 255;
-		var colorIndex = Math.floor(tick / 2000) % 6;
-		var color = this.getColor(colorIndex, colorDegree, subColorDegree);
-
-		var target = -140;
-		var translateY = target * halfCurvedRatio;
+		const target = -140;
+		const translateY = target * halfCurvedRatio;
 
 		context.translate(0, translateY + 20);
 
-		var alpha = sqrtCurvedRatio * 0.4;
+		const alpha = sqrtCurvedRatio * 0.4;
 
 		context.globalAlpha = alpha;
 
@@ -199,12 +207,12 @@ class HanulseBigWarpEffectRenderer {
 		context.lineTo(offsetX, -offsetY);
 		context.lineTo(offsetX + 4, -offsetY);
 		context.lineTo(offsetX + 4, height - offsetY);
-		context.fillStyle = this.ringGradient;
+		context.fillStyle = this.#ringGradient;
 		context.fill();
 		context.closePath();
 	}
 
-	getColor(index, colorDegree, subColorDegree) {
+	#getColor(index, colorDegree, subColorDegree) {
 		if (index == 0) {
 			return "rgba(" + colorDegree + ", 255, 255, 0.8)";
 		} else if (index == 1) {
