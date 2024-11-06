01950ef0a9bd52b99aac7c63ed4e32084d37bc8e
kosooyoul <kosooyoul@gmail.com>
2024-11-06 22:48:08 +0900

    Upt. url google login

--- js/HanulseSns.js
+++ js/HanulseSns.js
@@ -5,6 +5,7 @@ class HanulseSns {
 		  `client_id=${encodeURIComponent('1006587893490-8rl2rmm19p43e0g0umkils8hpfm4v0jl.apps.googleusercontent.com')}` +
 		  `&redirect_uri=${encodeURIComponent('https://www.auoi.net/sns/google')}` +
 		  '&response_type=code' +
+		  '&prompt=select_account' +
 		  '&scope=email%20profile&service=lso&o2v=2&theme=glif&flowName=GeneralOAuthFlow'
 		);
 	}
